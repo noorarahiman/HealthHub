ee7be955c5266def311d156b3762fd13a6824d3b
Noora Rahiman <noorarahiman2@gmail.com>
2026-01-17 10:04:55 +0530

    fix bookingform: include all fields properly and keep booked_on in model

--- home/forms.py
+++ home/forms.py
@@ -1,25 +1,22 @@
 from django import forms
 from .models import Booking
 
-
 class DateInput(forms.DateInput):
     input_type = 'date'
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = '__all__',
+        fields = ['p_name', 'p_phone', 'p_email', 'doc_name', 'booking_date']  # all editable fields
 
-        
         widgets = {
             'p_name': forms.TextInput(attrs={'class': 'form-control'}),
             'p_phone': forms.TextInput(attrs={'class': 'form-control'}),
             'p_email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'doc_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'doc_name': forms.Select(attrs={'class': 'form-control'}),  # dropdown for ForeignKey
             'booking_date': DateInput(attrs={'class': 'form-control'}),
         }
 
-        
         labels = {
             'p_name': "Patient Name:",
             'p_phone': "Patient Phone:",
